1ec4cdb9cc3772286158467d5bc2fdbce0c469d8
William Mucheru <willyk99@gmail.com>
2022-02-18 11:17:24 +0300

    Cleanup

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -11,16 +11,6 @@ const myTodos = [
 
 const TodoContext = createContext([]);
 
-/**
- * 
- * Use useContext to manage Todo list app, where
- * 
- * - on clicking one item (<Todo/> component), it moves to the top of the list
- * 
- * Use useRefs to have text box add item to the list
- * 
-*/
-
 function Todo({ todo }) {
     const [todos, setTodos] = useContext(TodoContext);
 
@@ -67,7 +57,7 @@ export default function Todos() {
     const [todos, setTodos] = useState(()=> myTodos);
 
     const todoList = todoItems => {
-        if (todoItems === undefined && todoItems.length === 0) {
+        if (todoItems === undefined || todoItems.length === 0) {
             return <div style={{ color: 'red' }}>No items</div>;
         }
 
